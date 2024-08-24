f86b12b3aaf361f7d2a83c7dbcffd9cb2905b913
adisa14 <adisa.abass@fcmb.com>
2024-08-24 13:02:08 +0100

    removed them from app

--- App.tsx
+++ App.tsx
@@ -1,29 +1,24 @@
 import React from "react";
 import { StyleSheet } from "react-native";
-import useAppTheme from "./src/app/hooks/useAppTheme";
-import { AppColorTheme } from "./src/utils/interfaces/Theme.types"
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import Main from "./src/app/navigation/main";
 import { Provider } from "react-redux";
 import store from "./src/app/store/store";
 
 const App = () => {
-  const { appColors } = useAppTheme()
-  const style = styles(appColors)
 
   return (
     <Provider store={store}>
-      <GestureHandlerRootView style={style.container}>
+      <GestureHandlerRootView style={styles.container}>
         <Main />
       </GestureHandlerRootView>
     </Provider>
   )
 }
 
-const styles = (color: AppColorTheme) => StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: color.white,
   },
 })
 
