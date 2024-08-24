bb64a938edb09e90b31552240370212eefd165c1
adisa14 <adisa.abass@fcmb.com>
2024-08-24 13:26:12 +0100

    imported reanimated

--- App.tsx
+++ App.tsx
@@ -1,24 +1,33 @@
-import React from "react";
-import { StyleSheet } from "react-native";
+import React, { useEffect } from "react";
+import { Platform, StyleSheet } from "react-native";
+import useAppTheme from "./src/app/hooks/useAppTheme";
+import { AppColorTheme } from "./src/utils/interfaces/Theme.types"
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import Main from "./src/app/navigation/main";
+import SplashScreen from "react-native-splash-screen"
 import { Provider } from "react-redux";
 import store from "./src/app/store/store";
 
+if (Platform.OS === 'android') {
+  SplashScreen.show()
+}
 const App = () => {
+  const { appColors } = useAppTheme()
+  const style = styles(appColors)
 
   return (
     <Provider store={store}>
-      <GestureHandlerRootView style={styles.container}>
+      <GestureHandlerRootView style={style.container}>
         <Main />
       </GestureHandlerRootView>
     </Provider>
   )
 }
 
-const styles = StyleSheet.create({
+const styles = (color: AppColorTheme) => StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: color.white,
   },
 })
 
